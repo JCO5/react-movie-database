5a03bc6b5167a415c627a9239608d619a4ff6a6f
ChamJet <jarrett.cham@gmail.com>
2023-10-13 14:32:02 -0700

    hit the griddy on single page

--- movie-database/src/components/Single.jsx
+++ movie-database/src/components/Single.jsx
@@ -39,26 +39,35 @@ const Single = () => {
   };
 
   return (
-    <section className="card">
-    <div className="grid-cols-2 group-relative">
+    <section className="card grid grid-cols-1 md:grid-cols-2 !p-0">
+
       {(single != undefined && single != null && single != '') && (
         <>
+        <div className="group-relative">
           {single.poster_path ? (
-            <img className="col-start-1 col-span-1" src={`https://image.tmdb.org/t/p/original${single?.poster_path}`} alt={single.title} />
+            <img className="relative inline-block p-8" src={`https://image.tmdb.org/t/p/original${single?.poster_path}`} alt={single.title} />
           ) : (
-            <div className='filler-poster'></div>
+            <img src="../src/image/filler-poster.svg" alt={single.title} />
           )}
           <button className="btn" onClick={() => toggleFavorites(single)}>
-            {isFavorite ? 'Remove from favorites' : 'Add to favorites' }
+            {isFavorite ?  <svg className="w-14 h-auto absolute top-28 fill-red-700 stroke-black" clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path d="m12 5.72c-2.624-4.517-10-3.198-10 2.461 0 3.725 4.345 7.727 9.303 12.54.194.189.446.283.697.283s.503-.094.697-.283c4.977-4.831 9.303-8.814 9.303-12.54 0-5.678-7.396-6.944-10-2.461z" fillRule="nonzero"/>
+                </svg> :
+                <svg className="w-14 h-auto absolute top-28 fill-white stroke-black" clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path d="m12 5.72c-2.624-4.517-10-3.198-10 2.461 0 3.725 4.345 7.727 9.303 12.54.194.189.446.283.697.283s.503-.094.697-.283c4.977-4.831 9.303-8.814 9.303-12.54 0-5.678-7.396-6.944-10-2.461z" fillRule="nonzero"/>
+                </svg> }
           </button>
+        </div>
+        <div className="md:col-start-2">
           <h2 className="flex-auto">{single.title}</h2>
           <p>{formatDate(single.release_date)}</p>
           {/* Called the conditional here */}
-          <p className="flex-auto">{roundVoteAverage(single.vote_average)}</p>
+          <p className="absolute top-32 left-4 bg-green-500 w-12 text-white rounded">{roundVoteAverage(single.vote_average)}</p>
           <p className="flex-none">{single.overview}</p>
+        </div>
         </>
       )}
-    </div>
+    
   </section>
   );
 };
