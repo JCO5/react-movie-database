b6242f8a93bcc7bf712949dcfc25410e7cf7d1d9
jestebanito <137816026+jestebanito@users.noreply.github.com>
2023-10-13 11:17:48 -0700

    added filler poster path image and all movie cards have the same size format

--- movie-database/src/pages/Search.jsx
+++ movie-database/src/pages/Search.jsx
@@ -24,7 +24,7 @@ export const Search = () => {
       <input
         type="search"
         placeholder="Search for a movie"
-        className= "search-text border-2 border-gray-300 bg-white h-10 px-10 pr-20 rounded-full text-5xl focus:outline-none mt-16"
+        className= "text-black border-2 border-gray-300 bg-white h-10 px-10 pr-20 rounded-full text-5xl focus:outline-none mt-16"
         value={query}
         onChange={onChange}
       />
