62923b7b9acd08ee726edc3102b1e56436140004
Joaquin Opulencia <joaquinopulencia99@gmail.com>
2023-10-13 13:12:02 -0700

    about page

--- movie-database/src/pages/About.jsx
+++ movie-database/src/pages/About.jsx
@@ -2,34 +2,43 @@ import React from 'react';
 
 function About() {
   return (
-    <div className="about-page h-screen flex justify-start flex-col p-16">
-      <h1 className='heading'>
-        For the Movie Lovers
-      </h1>
-      <p>
-        Find movies you’ve been meaning to see. movies you’re excited to watch, and reminisce on movies you love.
-      </p>
-      <p>
-        JJJ Movies is an API-based movie database. We use the TMDb API for our content generation but we are not affiliated with TMDb in any form
-      </p>
-      <p>
-        An API-based Movie Database
-      </p>
-      <p>
-        For the Movie Lover in You  
-      </p>
-      <h2 className=''>Our Team</h2>
-      <ul>
-        <li>Jet Cham</li>
-        <li>Joaquin Opulencia</li>
-        <li>Josh Esteban</li>
-      </ul>
-      <h2>Contact Us</h2>
-      <p>
-        If you have any questions or would like to learn more about our
-        services, please don't hesitate to contact us. We are here to assist
-        you.
-      </p>
+    <div className="about-page h-screen p-16 backdrop-filter relative">
+      <div className="h-screen w-full bg-[url('../image/about-us-image.jpg')] blur absolute top-0 left-0"></div>
+      <div className='relative z-20'>
+        <section>
+          <h1 className='heading'>
+            For the Movie Lovers
+          </h1>
+          <p>
+            Find movies you’ve been meaning to see. movies you’re excited to watch, and reminisce on movies you love.
+          </p>
+          <p>
+            JJJ Movies is an API-based movie database. We use the TMDb API for our content generation but we are not affiliated with TMDb in any form
+          </p>
+          <p>
+            An API-based Movie Database
+          </p>
+          <p>
+            For the Movie Lover in You  
+          </p>
+        </section>
+        <section>
+          <h2 className=''>Our Team</h2>
+          <ul>
+            <li>Jet Cham</li>
+            <li>Joaquin Opulencia</li>
+            <li>Josh Esteban</li>
+          </ul>
+        </section>
+        <section>
+          <h2>Contact Us</h2>
+          <p>
+            If you have any questions or would like to learn more about our
+            services, please don't hesitate to contact us. We are here to assist
+            you.
+          </p>
+        </section>
+      </div>
     </div>
   );
 }
--- movie-database/src/pages/Favorites.jsx
+++ movie-database/src/pages/Favorites.jsx
@@ -1,13 +1,14 @@
 import React, { useContext } from 'react'
 import { GlobalContext } from '../context/GlobalState';
 import MovieCard from '../components/MovieCard';
+import { Link } from 'react-router-dom';
 
 export const Favorites = () => {
   const { favorites } = useContext(GlobalContext);
   
   return (
     <div>
-      <h1 className='heading pt-16'>Your Favorites</h1>
+      <h1 className='heading pt-16'>Your Favorites ({favorites.length}) </h1>
       {favorites.length > 0 ? (
         <div className="card grid grid-cols-1 md:grid-cols-4">
           {favorites.map((movie) => (
@@ -16,7 +17,8 @@ export const Favorites = () => {
         </div>
       ) : (
         <div className='h-screen'>
-          <h2 className="no-movies">No movies in your list! Add some!</h2>
+          <h2 className="no-movies">There are no movies in your list!</h2>
+          <Link class="text-green-500 hover:text-blue-500 transition-colors" to = "/#">Add Some!</Link>
         </div>
       )}
     </div>
